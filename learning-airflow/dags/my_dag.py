386144b502b1601d0aea2a9c07787eb1f6b1d294
islam <mohammadislamansari750@gmail.com>
2025-02-19 17:49:35 +0530

    First Commit of Airflow project

---
+++ learning-airflow/dags/my_dag.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python import PythonOperator
+
+def print_a():
+    print('Hello World!')
+
+def print_b():
+    print('Hello World!')
+
+def print_c():
+    print('Hello World!')
+
+def print_d():
+    print('Hello World!')
+
+with DAG('my_dag', start_date=datetime(2024, 2, 19),
+         description='A simple tutorial DAG',
+         tags=['Data Science'],
+         schedule='@daily',
+         catchup=False): 
+    
+    task_a = PythonOperator(task_id='task_a', python_callable=print_a)
+    task_b = PythonOperator(task_id='task_b', python_callable=print_a)
+    task_c = PythonOperator(task_id='task_c', python_callable=print_a)
+    task_d = PythonOperator(task_id='task_d', python_callable=print_a)
+
+
+    task_a >> [task_b, task_c] >> task_d
