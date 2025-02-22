8d3c7e2815d30cdf9e11d3fa1a60ee2401ab890c
Islam-ansari <mohammadislamansari88@gmail.com>
2025-02-22 09:33:41 +0530

    rectified changes to my_dag file

--- learning-airflow/dags/my_dag.py
+++ learning-airflow/dags/my_dag.py
@@ -21,9 +21,9 @@ with DAG('my_dag', start_date=datetime(2024, 2, 19),
          catchup=False): 
     
     task_a = PythonOperator(task_id='task_a', python_callable=print_a)
-    task_b = PythonOperator(task_id='task_b', python_callable=print_a)
-    task_c = PythonOperator(task_id='task_c', python_callable=print_a)
-    task_d = PythonOperator(task_id='task_d', python_callable=print_a)
+    task_b = PythonOperator(task_id='task_b', python_callable=print_b)
+    task_c = PythonOperator(task_id='task_c', python_callable=print_c)
+    task_d = PythonOperator(task_id='task_d', python_callable=print_d)
 
 
     task_a >> [task_b, task_c] >> task_d
